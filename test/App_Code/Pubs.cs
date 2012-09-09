16b57f457cfcb2c5f1e662a45f582c8742e91506
Todd Qualls (Enoch) <tqualls@gmail.com>
2012-09-09 15:09:12 -0400

    WIP on calculating royalties.

--- test/App_Code/Pubs.cs
+++ test/App_Code/Pubs.cs
@@ -12,7 +12,9 @@ public class Pubs
 {
     public class _Royalty
     {
-        public string StoreOrderID { get; set; }
+        public string StoreName { get; set; }
+        public string OrderNumber { get; set; }
+        public string OrderDate { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Royalty {get;set;}
@@ -21,7 +23,42 @@ public class Pubs
     public static List<_Royalty> GetRoyalties(string pubID)
     {
         List<_Royalty> royalties = new List<_Royalty>();
+        publisher pub = new publisher();
+        decimal saleAmount = 0;
+        store store = new store();
+        author author = new author();
+        roysched roysched = new roysched();
+        using (pubsEntities context = new pubsEntities())
+        {
+            //First get the publisher selected
+            pub = context.publishers.FirstOrDefault(x => x.pub_id == pubID);
+            //Get list of all sales for the selected pub
+            IEnumerable<sale> orders = context.sales.Where(x => x.title.pub_id == pubID);
+            //Iterate through the orders and calculate the royalty for each
+            foreach (sale o in orders)
+            {
+                _Royalty r = new _Royalty();
+                r.StoreName = o.store.stor_name;
+                r.OrderNumber = o.ord_num;
+                r.OrderDate = o.ord_date.ToShortDateString();
+                r.Title = o.title.title1;
+                title t = context.titles.FirstOrDefault(x => x.title_id == o.title_id);
+ 
+                
+                if (o.title.price != null)
+                {
+                    saleAmount = (decimal)o.qty * Convert.ToDecimal(o.title.price);
+                    
+                }
+                else
+                {
+                    //No price for the current title is set in the db, so royalty cannot be computed
+                    
+                }
+            }
 
+        }
+        
 
         return royalties;
     }
