99fe30d99e9f492113df0af5783f0efff2a76e40
Todd Qualls <tqualls@gmail.com>
2012-09-11 16:10:53 -0400

    Tweaks authentication method.

--- test/App_Code/Account.cs
+++ test/App_Code/Account.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 
 /// <summary>
 /// Functions and info related to login and authentication
@@ -21,11 +22,21 @@ public class Account
 
     public static bool AuthenticateUser(string userName, string email, string password)
     {
-        //first validate the email address
+        StringBuilder error = new StringBuilder();
+        bool eCaught = false;
+        //make sure name not blank
+        if(!Validate.IsFieldPopulated(userName.Trim()))
+        {
+            eCaught = true;
+            error.Append("You must provide your name before logging in. ");
+        }
+        //validate the email address
         if (!Validate.IsValidEmail(email))
         {
-            throw new Exception("The e-mail address provided is not valid. You must provide a valid email address before logging-in.");
+            eCaught = true;
+            error.Append("The e-mail address provided is not valid. You must provide a valid email address before logging-in.");
         }
+        if(eCaught) throw new Exception(error.ToString());
         if (password == PASSWORD)
         {
             User user = new User();
